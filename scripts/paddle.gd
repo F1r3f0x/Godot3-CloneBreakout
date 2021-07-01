c606b7bb8c866544cb0183c416d925f3a9559faf
Patricio Labin <plabin@outlook.cl>
2021-07-01 01:30:09 -0400

    move the paddle

--- scripts/paddle.gd
+++ scripts/paddle.gd
@@ -1,17 +1,13 @@
 extends KinematicBody2D
 
-var testvar = 'paddle'
+export var SPEED = 1000
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
+func _physics_process(delta):
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if Input.is_action_pressed("paddle_move_left"):
+		position.x -= SPEED * delta
+	if Input.is_action_pressed("paddle_move_right"):
+		position.x += SPEED * delta
